3ca2e426d48fd57d07263f7a7821ec93c829d2da
Zack <mcodikid@gmail.com>
2019-08-21 23:17:42 -0600

    Add possible electron support!

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ var element = require('./lib/element.js');
 var script = require('./lib/script.js');
 var port = require('./lib/port.js');
 var join = require('./lib/join.js');
+var temp = require('./lib/temp.js');
 
-/* Jeremy's Website */
-var header = new element('h1');
-header.text = 'Jeremy is (sometimes) awesome!';
-
-var para = new element('p');
-para.text = `Hello, world!  I am not Jeremy that is writing this, but hello anyway!`;
-
-var body = join('body', header, para);
-
-var port = new port();
-port.src(myBody)
-    .dest('./index.html')
-    .pipe();
+module.exports = {
+    element: element,
+    script: script,
+    port: port,
+    join: join,
+    temp: temp
+};
