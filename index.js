a042dacae68ebe79f4c1d12ba1d2613c4ab8122a
Zack <mcodikid@gmail.com>
2019-08-21 22:47:01 -0600

    Fix bad Git.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var element = require('./lib/element.js');
 var script = require('./lib/script.js');
+var port = require('./lib/port.js');
 
 var myScript = new script();
 
@@ -11,4 +12,12 @@ var myDiv = new element('div');
 myDiv.text = myScript;
 myDiv.attributes.id = 'amazing-div-element';
 
-console.log(myDiv.compileHTML());
+var myBody = new element('body');
+myBody.attributes.id = 'body-nav';
+myBody.style.backgroundColor = 'black';
+myBody.style.color = 'white';
+
+var port = new port();
+port.src(myBody)
+    .dest('./index.html')
+    .pipe();
